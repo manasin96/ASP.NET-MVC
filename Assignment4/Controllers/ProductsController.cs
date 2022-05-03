16559e76dea6f035506931e607eb2b2cefd7ed4a
Manasi N <manasi.n@capgemini.com>
2022-05-03 12:00:10 +0530

    All MVC Assignments done here

---
+++ Assignment4/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAss4.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            Models.Product p = new Models.Product();
+            //pass a list of products to the view return View(p.GetProducts());
+            return View(p.GetProducts());
+        }
+    }
+}
